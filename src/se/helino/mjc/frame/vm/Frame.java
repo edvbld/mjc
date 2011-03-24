cccdf07e0e7415d141aa9f20464bb0f46ecd031b
helino <erik.helin@gmail.com>
2011-03-24 13:34:47 +0100

    updated the interfaces a little bit

--- src/se/helino/mjc/frame/vm/Frame.java
+++ src/se/helino/mjc/frame/vm/Frame.java
@@ -3,9 +3,9 @@ package se.helino.mjc.frame.vm;
 import se.helino.mjc.parser.MJType;
 
 public interface Frame {
-    public Access allocArgument(String id, MJType type);
-    public Access allocLocal(String id, MJType type);
-    public int numberOfArguments();
+    public void allocArgument(String id, MJType type);
+    public void allocLocal(String id, MJType type);
     public int numberOfLocals();
-    public String procEntry();
+    public Access getLocal(String name);
+    public Access getArgument(String name);
 }
--- src/se/helino/mjc/frame/vm/Record.java
+++ src/se/helino/mjc/frame/vm/Record.java
@@ -3,6 +3,7 @@ package se.helino.mjc.frame.vm;
 import se.helino.mjc.parser.MJType;
 
 public interface Record {
-    public Access allocField(String id, MJType type);
-    public int numberOfFields();
+    public void allocField(String id, MJType type);
+    public Frame getMethod(String name);
+    public Access getField(String name);
 }
