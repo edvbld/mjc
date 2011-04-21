ff37a33d8c139b6c1569902a7e4b4b7247f913d8
helino <erik.helin@gmail.com>
2011-04-21 13:00:59 +0200

    added runner for the negative tests

--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "POSITIVE TESTS"
+echo "=============="
 positive=`ls tests/positive/*.java` 
 for fname in $positive
 do
@@ -16,4 +18,22 @@ do
     fi
     cd ..
     rm -r "$name"
+done
+
+echo "NEGATIVE TESTS"
+echo "=============="
+negative=`ls tests/negative/*.java` 
+for fname in $negative
+do
+    java -jar dist/mjc.jar -q $fname > output
+    name=`basename ${fname%\.*}`
+    output="${fname%\.*}.output"
+    if `cmp -s output ${output} > /dev/null`; then
+        echo "$name: OK"
+    else
+        echo "$name: FAIL"
+        exit 1
+    fi
+    rm -r "$name"
+    rm "output"
 done
